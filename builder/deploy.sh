09f9727c9cd7216d027c29acefd2fbcbb8850032
Ian Kirker <ian.kirker@gmail.com>
2016-10-24 19:28:52 +0100

    Adds explicit bash interpreter command to deploy script

--- builder/deploy.sh
+++ builder/deploy.sh
@@ -8,7 +8,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  builder/build.sh
+  bash builder/build.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
